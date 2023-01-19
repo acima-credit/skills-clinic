8a80761aced9c28e7b76967015fbcda8a7878681
Tad Thorley <phaedryx@gmail.com>
2023-01-19 11:23:16 -0700

    preview of grouping information for useState

--- 2023/1/email-signature/app/javascript/react/components/App.jsx
+++ 2023/1/email-signature/app/javascript/react/components/App.jsx
@@ -1,12 +1,22 @@
 import * as React from "react"
+import { useState } from "react";
 import Form from "./Form.jsx"
 import Preview from "./Preview.jsx"
 
 const App = (props) => {
+  const [employeeInfo, setEmployeeInfo] = useState({ fullName: ""})
+
   return(
-    <div>
-      <Form />
-      <Preview />
+    <div className="container">
+      <p>Email Signature</p>
+      <div className="row">
+        <div className="col">
+          <Form employeeInfo={ employeeInfo} setEmployeeInfo={ setEmployeeInfo } />
+        </div>
+        <div className="col">
+          <Preview employeeInfo={ employeeInfo } />
+        </div>
+      </div>
     </div>
   )
 }
--- 2023/1/email-signature/app/javascript/react/components/Form.jsx
+++ 2023/1/email-signature/app/javascript/react/components/Form.jsx
@@ -1,26 +1,33 @@
 import * as React from "react"
 
 const Form = (props) => {
+  const handleEmployeeInfoChange = (event) => {
+    const { id, value } = event.target
+    console.log(id)
+    console.log(value)
+    console.log(props.employeeInfo)
+    props.setEmployeeInfo({...props.employeeInfo, [id]: value })
+  }
+
   return(
     <section>
       Form
       <div className="row mb-3">
         <label htmlFor="full-name" className="col-sm-2 col-form-label">Full Name*</label>
-        <div class="col-sm-10">
-          <input type="text" className="form-control" id="full-name" />
+        <div className="col-sm-10">
+          <input type="text" className="form-control" id="fullName" value={ props.employeeInfo.fullName } onChange={ handleEmployeeInfoChange } />
         </div>
       </div>
       <div className="row mb-3">
         <label htmlFor="title" className="col-sm-2 col-form-label">Title</label>
-        <div class="col-sm-10">
+        <div className="col-sm-10">
           <input type="text" className="form-control" id="title" />
         </div>
       </div>
       <div className="row mb-3">
         <label htmlFor="company" className="col-sm-2 col-form-label">Company*</label>
-        <div class="col-sm-10">
+        <div className="col-sm-10">
           <select className="form-select" id="company">
-            <option selected>Choose...</option>
             <option value="1">One</option>
             <option value="2">Two</option>
           </select>
@@ -28,13 +35,13 @@ const Form = (props) => {
       </div>
       <div className="row mb-3">
         <label htmlFor="email" className="col-sm-2 col-form-label">Email*</label>
-        <div class="col-sm-10">
+        <div className="col-sm-10">
           <input type="text" className="form-control" id="email" />
         </div>
       </div>
       <div className="row mb-3">
         <label htmlFor="phone" className="col-sm-2 col-form-label">Phone</label>
-        <div class="col-sm-10">
+        <div className="col-sm-10">
           <input type="text" className="form-control" id="phone" />
         </div>
       </div>
