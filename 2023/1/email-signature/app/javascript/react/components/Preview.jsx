d5990fdd151931d8ebe1040565dfe3872ee38534
Tad Thorley <phaedryx@gmail.com>
2023-01-19 11:23:16 -0700

    preview of grouping information for useState

--- 2023/1/email-signature/app/javascript/react/components/Preview.jsx
+++ 2023/1/email-signature/app/javascript/react/components/Preview.jsx
@@ -2,7 +2,16 @@ import * as React from "react"
 
 const Preview = (props) => {
   return(
-    <div>Preview</div>
+    <section>
+      <div>Preview</div>
+      <div>
+        <div><b>{ props.employeeInfo.fullName }</b></div>
+        <div><i>{ props.title }</i><br /></div>
+        <p>Company Info</p>
+        <div>{ props.email }</div>
+        <div>{ props.phone }</div>
+      </div>
+    </section>
   )
 }
 
