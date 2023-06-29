33c56fea9a6aac5812f1baa111f027fd4ff4fb6e
hmnelson13 <haley.nelson@acima.com>
2023-06-29 13:26:39 -0600

    fixing day 1 rubocop

--- 2023/advent-2022/1/lib/calorie_maximizer.rb
+++ 2023/advent-2022/1/lib/calorie_maximizer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Maximize Calories
 class CalorieMaximizer
   def initialize(file_path, elves: 1)
     @file_path = file_path
@@ -7,16 +8,16 @@ class CalorieMaximizer
   end
 
   def call
-    File.open(@file_path, "r")
-      .each_line
-      .lazy
-      .map(&:to_i)
-      .chunk(&:positive?)
-      .filter(&:all?)
-      .map(&:last)
-      .map(&:sum)
-      .sort
-      .last(@elves)
-      .sum || 0
+    File.open(@file_path, 'r')
+        .each_line
+        .lazy
+        .map(&:to_i)
+        .chunk(&:positive?)
+        .filter(&:all?)
+        .map(&:last)
+        .map(&:sum)
+        .sort
+        .last(@elves)
+        .sum || 0
   end
 end
--- 2023/advent-2022/1/spec/calorie_maximizer_spec.rb
+++ 2023/advent-2022/1/spec/calorie_maximizer_spec.rb
@@ -1,45 +1,59 @@
+# frozen_string_literal: true
+
 require_relative '../lib/calorie_maximizer'
 require 'pathname'
 
 RSpec.describe CalorieMaximizer do
-	let(:valid_file_path) { Pathname.new(__dir__).join("data", "calories.txt") }
-	let(:file) {  valid_file_path }
-	let(:args) { [file] }
+  let(:valid_file_path) { Pathname.new(__dir__).join('data', 'calories.txt') }
+  let(:file) { valid_file_path }
+  let(:args) { [file] }
 
-	describe ".new" do
-		subject { described_class.new(*args) }
+  describe '.new' do
+    subject { described_class.new(*args) }
 
-		context "without args" do
-			let(:args) { [] }
-			it("fails") { expect { subject }.to raise_error(ArgumentError) }
-		end
+    context 'without args' do
+      let(:args) { [] }
+      it('fails') { expect { subject }.to raise_error(ArgumentError) }
+    end
 
-		context "with args" do
-			it "succeeds" do
-				subject
-			end
-		end
-	end
+    context 'with args' do
+      it 'succeeds' do
+        subject
+      end
+    end
+  end
 
-	describe "#call" do
-		let(:kwargs) { {} }
-		subject { described_class.new(*args, **kwargs).call }
+  describe '#call' do
+    let(:kwargs) { {} }
+    subject { described_class.new(*args, **kwargs).call }
 
-		context "when empty file" do
-			let(:valid_file_path) { Pathname.new(__dir__).join("data", "empty_calories.txt") }
+    context 'when empty file' do
+      let(:valid_file_path) { Pathname.new(__dir__).join('data', 'empty_calories.txt') }
 
-			it { is_expected.to eq(0) }
-		end
+      it { is_expected.to eq(0) }
+    end
 
-		context "when non-empty file" do
-			it { is_expected.to eq(24000) }
+    context 'when non-empty file' do
+      it { is_expected.to eq(24_000) }
 
-			context "with number of elves as an arg" do
-				let(:args) { [file] }
-				let(:kwargs) { { elves: 3 } }
+      context 'with full file' do
+        let(:valid_file_path) { Pathname.new(__dir__).join('data', 'full.data.txt') }
 
-				it { is_expected.to eq(45000) }
-			end
-		end
-	end
+        it { is_expected.to eq(72_511) }
+      end
+
+      context 'with number of elves as an arg' do
+        let(:args) { [file] }
+        let(:kwargs) { { elves: 3 } }
+
+        it { is_expected.to eq(45_000) }
+
+        context 'with full file' do
+          let(:valid_file_path) { Pathname.new(__dir__).join('data', 'full.data.txt') }
+
+          it { is_expected.to eq(212_117) }
+        end
+      end
+    end
+  end
 end
