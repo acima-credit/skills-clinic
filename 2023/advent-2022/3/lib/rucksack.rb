e3152eac1f57c2400e0a70ca22ebc4a9fae974e9
hmnelson13 <haley.nelson@acima.com>
2023-06-30 17:32:18 -0600

    Cleaned up day 3 as well

--- 2023/advent-2022/3/lib/rucksack.rb
+++ 2023/advent-2022/3/lib/rucksack.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# stuff
+# Rucksack
 class Rucksack
   Error     = Class.new(StandardError)
   FileError = Class.new(Error)
@@ -13,22 +13,22 @@ class Rucksack
   end
 
   def call
-    @version == 1 ? get_data : get_badge
+    @version == 1 ? data : badge
   end
 
   private
 
-  def get_data
-    File.open(@file_path, "r")
-      .each_line
-      .lazy
-      .map(&:chars) # e.g. [by rucksack[v,J,r,...],[j,q,H,...]]
-      .map(&method(:split_line)) #[[[v,J,r,...],[...]],[[j,q,H,...],[...]]]
-      .map{ |row| row.reduce(&:&) } # Returns an array of enumerators e.g [Enumerator::Lazy]
-      .force # Returns an array of non-lazy enumerators e.g [["p"], ["L"], ["P"], ["v"], ["t"], ["s"]]
-      .flatten # Returns a single array by reducing the dimensionality. e.g ["p", "L", "P", "v", "t", "s"]
-      .map(&method(:get_priority)) # Returns the ASCII values per each letter e.g [16, 38, 42, 22, 20, 19]
-      .sum # Adds up the values of the array
+  def data
+    File.open(@file_path, 'r')
+        .each_line
+        .lazy
+        .map(&:chars) # e.g. [by rucksack[v,J,r,...],[j,q,H,...]]
+        .map(&method(:split_line)) # [[[v,J,r,...],[...]],[[j,q,H,...],[...]]]
+        .map { |row| row.reduce(&:&) } # Returns an array of enumerators e.g [Enumerator::Lazy]
+        .force # Returns an array of non-lazy enumerators e.g [["p"], ["L"], ["P"], ["v"], ["t"], ["s"]]
+        .flatten # Returns a single array by reducing the dimensionality. e.g ["p", "L", "P", "v", "t", "s"]
+        .map(&method(:get_priority)) # Returns the ASCII values per each letter e.g [16, 38, 42, 22, 20, 19]
+        .sum # Adds up the values of the array
   end
 
   def get_priority(letter)
@@ -37,21 +37,19 @@ class Rucksack
 
   def split_line(str)
     mid = str.size / 2
-    [str[0, mid], str[mid..]]
+    [str[0, mid], str[mid..-1]]
   end
 
-  def get_badge
+  def badge
     File.open(@file_path, 'r')
-      .each_line
-      .each_slice(3)
-      .lazy
-      .map{ |sack| sack.map(&:strip).map(&:chars) }
-      .map{ |row| row.reduce(&:&) }
-      .force
-      .flatten
-      .map(&method(:get_priority))
-      .sum
+        .each_line
+        .each_slice(3)
+        .lazy
+        .map { |sack| sack.map(&:strip).map(&:chars) }
+        .map { |row| row.reduce(&:&) }
+        .force
+        .flatten
+        .map(&method(:get_priority))
+        .sum
   end
 end
-
-puts Rucksack.new(file_path: './spec/data/sample3.data.txt', version: 2).call
--- 2023/advent-2022/3/spec/rucksack_spec.rb
+++ 2023/advent-2022/3/spec/rucksack_spec.rb
@@ -5,38 +5,48 @@ require 'pathname'
 
 RSpec.describe Rucksack do
   let(:kwargs) { { file_path: file_path, version: version } }
-  let(:file_path) { Pathname.new(__dir__).join("data", "sample2.data.txt") }
+  let(:file_path) { Pathname.new(__dir__).join('data', 'sample.data.txt') }
   let(:version) { 1 }
 
-  describe "#new" do
+  describe '#new' do
     subject { described_class.new(**kwargs) }
 
-    context "when file_path invalid" do
-      let(:file_path) { "badpath" }
+    context 'when file_path invalid' do
+      let(:file_path) { 'badpath' }
 
-      it "raises a file error" do
+      it 'raises a file error' do
         expect { subject }.to raise_error(described_class::FileError)
       end
     end
 
-    context "when file_path valid" do
+    context 'when file_path valid' do
       it "doesn't raise a file error" do
         expect { subject }.to_not raise_error
       end
     end
   end
 
-  describe "#call" do
+  describe '#call' do
     subject { described_class.new(**kwargs).call }
 
-    context "with part 1 rules" do
-      it { is_expected.to eq 157}
+    context 'with part 1 rules' do
+      it { is_expected.to eq 157 }
+
+      context 'with full dataset' do
+        let(:file_path) { Pathname.new(__dir__).join('data', 'input.data.txt') }
+        it { is_expected.to eq 7980 }
+      end
     end
 
-    context "with part 2 rules" do
+    context 'with part 2 rules' do
       let(:version) { 2 }
 
       it { is_expected.to eq 70 }
+
+      context 'with full dataset' do
+        let(:file_path) { Pathname.new(__dir__).join('data', 'input.data.txt') }
+        it { is_expected.to eq 2881 }
+      end
     end
   end
 end
